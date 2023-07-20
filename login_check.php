324982d5f6755bd91de8d7146e80560e1f75192d
yonatan-h <99685585+yonatan-h@users.noreply.github.com>
2023-07-20 15:39:46 +0300

    Update login_check.php

--- login_check.php
+++ login_check.php
@@ -3,14 +3,10 @@
     $password = $_POST['password'];
     $email = $_POST['email'];
 
-     /// This looks vulnerable to SQL injection. For example, attacker can submit an email="fake@gmail.com'; DROP TABLE Users; -- ".
-     /// This can be prevented by using prepared statements.
-     /// You can read about it here: https://www.w3schools.com/php/php_mysql_prepared_statements.asp 
 
     $sql = "SELECT * FROM users WHERE email = '$email' AND password = '$password'";
     $result = mysqli_query($conn, $sql);
     if(filter_var($email,FILTER_VALIDATE_EMAIL)){
-     ///This block is empty. If the intension is to fill this out later, it is better if you leave a TODO comment. https://www.jetbrains.com/help/phpstorm/using-todo.html
         	}
         	else{
                
@@ -19,8 +15,6 @@
         	}
     // Check if user exists
     if (mysqli_num_rows($result) > 0) {
-         /// It is a nice comment to have. It is informative for people who are not fammiliar with php like me.
-         // User exists, redirect to another page
      
         header("Location: sell_page.php");
         exit();

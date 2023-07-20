c81de0eb799d3d2223527bca4752c43a46e242e2
yonatan-h <99685585+yonatan-h@users.noreply.github.com>
2023-07-20 15:56:37 +0300

    Update login_check.php

--- login_check.php
+++ login_check.php
@@ -3,18 +3,22 @@
      require "connection.php";
     $password = $_POST['password'];
     $email = $_POST['email'];
-    
+
+
     $sql = "SELECT * FROM users WHERE email = '$email' AND password = '$password'";
     $result = mysqli_query($conn, $sql);
     if(filter_var($email,FILTER_VALIDATE_EMAIL)){
+         
         		
         	}
         	else{
+               
                 echo '<script>alert("Incorrect Email or Password ");</script>';
 
         	}
     // Check if user exists
     if (mysqli_num_rows($result) > 0) {
+         
     // User exists, redirect to another page
         header("Location: sell_page.php");
         exit();
